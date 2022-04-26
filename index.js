5b1c3ce5b0a38aaeec82b6da00b5c17ca6555c08
Madseffect <themadsk@gmail.com>
2022-04-26 19:34:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ document.addEventListener('mousemove', (event) => {
     document.body.style.backgroundColor = `rgb(${event.clientX}, ${event.clientY}, ${event.clientX - event.clientY})`;
 
 });
+
+window.addEventListener("deviceorientation", (event2)=>{
+	document.body.style.backgroundColor = `rgb(${event.alpha}, ${event.beta}, ${event.gamma})`;
+});
+
